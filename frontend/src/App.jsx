303508605cd5b8f8949618d946c9d83c0f4b8d44
kushalswami20 <161000509+kushalswami20@users.noreply.github.com>
2024-10-26 12:30:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css'
 import Home from './components/Home';
-import SearchClaimAgent from './components/SearchClaimAgent';
 import ChatbotPage from './components/ChatBot';
-import KnowledgeAgent from './components/KnowledgeAgent';
-// import ImageUpload from './components/StoringData';
 import MetaMaskConnector from './components/MetaMaskConnector';
 import AddUserForm from './components/AddUserForm';
 import UserProfile from './components/Profile';
@@ -20,9 +16,7 @@ function App() {
     <Router>
       <Routes>
       <Route path="/home" element= {<Home />} />
-      <Route path="/searchclaimagent" element={<SearchClaimAgent />} />
       <Route path="/chatbot" element={<ChatbotPage />} />
-      <Route path="/knowledgeagent" element={<KnowledgeAgent />} />
       <Route path="/adduser" element={<AddUserForm />} />
       {/* <Route path='/storingdata' elemnt={< ImageUpload />} /> */}
       <Route path ='/profile' element ={<UserProfile />} />
